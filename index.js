2fd151461cdc45892cd3944c88be9bab07ee731b
Jhalak <stdjhalak@gmail.com>
2024-08-17 16:52:53 +0600

    some error fix

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(port, () => {
-    console.log(`Catalog Master Server is running on port : ${port}`)
+    console.log(`Catalog Master Server is running on port : ${port}`);
 })
