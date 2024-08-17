a6e318f33872eb7b2489c59a6790b9f414c6fdec
Jhalak <stdjhalak@gmail.com>
2024-08-17 17:29:28 +0600

    error fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 const corsOptions={
     origin:['http://localhost:5173'],
     credentials:true,
-    optionSuccessStatus:200
+    optionSuccessStatus:200,
 }
 
 app.use(cors(corsOptions));
